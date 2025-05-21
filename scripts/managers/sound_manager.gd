4f6973f35d6114cfe1d07ccfb47bff237b4724ea
Ryan Anan <ryanda.mahdy@ui.ac.id>
2025-05-21 12:58:42 +0700

    add base implementation of sound manager

---
+++ scripts/managers/sound_manager.gd
@@ -0,0 +1,36 @@
+extends Node2D
+## Node to manage sound playing through the game
+##
+## put `SoundManager.play_sfx(AudioStream) to play sfx, `SoundManager.play_bgm(AudioStreamMP3) to set BGM
+## sfx is one and done, and can be set to change pitch each initiation with the random flag
+## bgm is a set and it loops until it's set to none or null
+##
+
+@onready var sfx_player : AudioStreamPlayer
+@onready var bgm_player : AudioStreamPlayer
+
+func play_sfx(sfx: AudioStream, random = false):
+	if sfx:
+		sfx_player = AudioStreamPlayer.new()
+		add_child(sfx_player)
+		sfx_player.set_process_mode(1)
+		
+		sfx_player.stream = sfx
+		sfx_player.bus = "SFX"
+		
+		if random:
+			sfx_player.set_pitch_scale(randf_range(0.75,2))
+		sfx_player.play()
+		
+		sfx_player.finished.connect(sfx_player.queue_free)
+		
+func play_bgm(bgm: AudioStreamMP3):
+	if bgm:
+		bgm.loop = true
+		if !bgm_player:
+			bgm_player = AudioStreamPlayer.new()
+			add_child(bgm_player)
+			bgm_player.set_process_mode(3)
+		bgm_player.stream = bgm
+		bgm_player.bus = "Music"
+		bgm_player.play()
